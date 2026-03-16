7c7ac015ab6fa888bc74e99dbea1207235b696fe
hyle <7681+hyle@users.noreply.github.com>
2026-03-16 22:11:17 +0100

    update fuoricode to v0.2.7

--- Formula/fuoricode.rb
+++ Formula/fuoricode.rb
@@ -1,8 +1,8 @@
 class Fuoricode < Formula
   desc "Export a codebase to a single Markdown file"
   homepage "https://github.com/hyle/fuoricode"
-  url "https://github.com/hyle/fuoricode/archive/refs/tags/v0.2.6.tar.gz"
-  sha256 "8afaab78ba0eec6d5de99d156f907ad59cdb7c956be4387eb7ea25af06e58c83"
+  url "https://github.com/hyle/fuoricode/archive/refs/tags/v0.2.7.tar.gz"
+  sha256 "8e897f0c6e2599eaa6c8bb5ff4ce23e897171c3dc4be64460a48d4b8179011bd"
   license "MIT"
 
   def install
